f643627ac0b041c017adba6bc789447e98574428
Alex Chetverov <alexu-che@yandex.ru>
2019-05-15 09:51:40 +0200

    Update reinstallmysql.sh

--- reinstallmysql.sh
+++ reinstallmysql.sh
@@ -2,6 +2,7 @@
 kill $(pgrep mysql)
 apt-get clean
 apt-get update
+dpkg -r phpmyadmin
 dpkg -r mysql-client-5.7
 dpkg -r mysql-server-5.7
 dpkg -r libmysqlclient20:i386
@@ -9,6 +10,7 @@ dpkg -r libmysqlclient20:amd64
 dpkg -r libmysqlclient18:amd64
 dpkg -r mysql-common
 dpkg -r mysql 
+apt-get remove --purge phpmyadmin
 apt-get remove --purge mysql-\*
 apt-get autoremove
 apt-get autoclean
@@ -16,9 +18,11 @@ apt-get install aptitude
 aptitude remove mysql-client 
 aptitude remove mysql-server
 aptitude remove mysql-common
+aptitude phpmyadmin
 rm -rf /etc/mysql
 rm -кf /var/lib/mysql
 deluser --remove-home mysqlrm -rf /var/log/mysql
 delgroup mysql
 find / -iname ‘mysql*’ -exec rm -rf {} \;
 sudo apt-get install mysql-common mysql-server* mysql-client*
+sudo apt-get phpmyadmin

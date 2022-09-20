687d7c5920e76687416cbd862dc6348904c2608c
Alex Chetverov <alexu-che@yandex.ru>
2022-09-20 13:51:48 +0300

    Update zadacha.py
    Новые названия папок

--- zadacha.py
+++ zadacha.py
@@ -23,7 +23,7 @@ else:
             z.write(',')
     else:
         if len(sys.argv)>1 and os.path.basename(os.getcwd())=="hochu5":
-            folders=["school","sch2107","informatika","Arduino","PythonGood","am_super","uchiurok","kabinet404","Shepkina","molodec","super"]
+            folders=["school","sch2107","informatika","Arduino","PythonGood","am_super","uchiurok","ITpolygon","Mikoyana","molodec","super"]
             baza_chisel=[[3542,2535,4588,469,7061,6127,972,1852,5271,6300],[4845,7176,6346,5538,6992,3971,4065,1914,4219,7658],[709,537,3269,5997,5872,2882,4786,5714,7706,2830],[1349,5948,4865,3458,6888,5895,5914,378,7652,5381],[7098,4157,6179,7549,3440,2334,1603,3288,882,4458],[6458,1238,2951,6263,2422,3725,6800,7758,2383,4831],[5482,7455,2678,6626,7125,597,1617,4581,5841,3948]]
             for i in range (7):
                 path=os.getcwd()

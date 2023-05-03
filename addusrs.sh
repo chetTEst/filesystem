058db64cc7eeb24acb55663f42356231f9ee7f28
Четверов Алексей <alexu-che@yandex.ru>
2023-05-03 14:29:49 +0300

    Update addusrs.sh
    Добавлена информация об ограничениях для пользователя

--- addusrs.sh
+++ addusrs.sh
@@ -2,6 +2,16 @@
 # This is a comment
 for ((i=1;i<40;i++))
 do
-useradd -c "uchenik$i" -d /home/uchenik$i -m -p "" -s /bin/bash uchenik$i
-echo uchenik$i:uchenik | chpasswd
+useradd -c "uchenik$i" -d /home/uchenik$i -m -p "" -s /bin/rbash uchenik$i
+echo uchenik$i:F1-KMGKyHN2GQ32smA1C | chpasswd
+chmod -R a-w /home/uchenik$i
+chown -R uchenik$i:uchenik$i /home/uchenik$i
+ln -s /bin/ls /path/to/limited_user_home/allowed_bin/ls
+ln -s /bin/cat /path/to/limited_user_home/allowed_bin/cat
+ln -s /bin/cd /path/to/limited_user_home/allowed_bin/cd
+ln -s /bin/cd /path/to/limited_user_home/allowed_bin/wc
+ln -s /bin/cd /path/to/limited_user_home/allowed_bin/pwd
+ln -s /bin/cd /path/to/limited_user_home/allowed_bin/cp
+ln -s /bin/cd /path/to/limited_user_home/allowed_bin/cp
+echo 'PATH=$HOME/allowed_bin' >> /home/uchenik$i/.bashrc
 done

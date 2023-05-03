c1e3a264fbd642a8b78d8849f74373d7837f6714
Четверов Алексей <alexu-che@yandex.ru>
2023-05-03 15:47:16 +0300

    Update addusrs.sh
    did stuf

--- addusrs.sh
+++ addusrs.sh
@@ -4,6 +4,8 @@ for ((i=1;i<8;i++))
 do
 useradd -c "uchenik$i" -d /home/uchenik$i -m -p "" -s /bin/rbash uchenik$i
 echo uchenik$i:F1-KMGKyHN2GQ32smA1C | chpasswd
+mkdir /home/uchenik$i/hochu5
+python3 /root/makeManyFile.py $i /home/uchenik$i/hochu5
 chmod -R a-w /home/uchenik$i
 chown -R uchenik$i:uchenik$i /home/uchenik$i
 ln -s /bin/ls /path/to/limited_user_home/allowed_bin/ls
@@ -15,4 +17,5 @@ ln -s /bin/cd /path/to/limited_user_home/allowed_bin/cp
 ln -s /bin/cd /path/to/limited_user_home/allowed_bin/cp
 ln -s /bin/cd /path/to/limited_user_home/allowed_bin/nano
 echo 'PATH=$HOME/allowed_bin' >> /home/uchenik$i/.bashrc
+
 done

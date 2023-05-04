17624851047191c5a66db420a0a56c006bd2b5f2
Четверов Алексей <alexu-che@yandex.ru>
2023-05-04 10:13:56 +0300

    Update addusrs.sh
    did stuff

--- addusrs.sh
+++ addusrs.sh
@@ -2,19 +2,18 @@
 # This is a comment
 for ((i=1;i<8;i++))
 do
-useradd -c "uchenik$i" -d /home/uchenik$i -m -p "" -s /bin/rbash uchenik$i
+useradd -c "uchenik$i" -d /home/filesystemTask/uchenik$i -m -p "" -s /bin/rbash uchenik$i
 echo uchenik$i:F1-KMGKyHN2GQ32smA1C | chpasswd
-mkdir /home/uchenik$i/hochu5
-python3 /root/makeManyFile.py $i /home/uchenik$i/hochu5
-chmod -R a-w /home/uchenik$i
-chown -R uchenik$i:uchenik$i /home/uchenik$i
-ln -s /bin/ls /home/uchenik$i/ls
-ln -s /bin/cat /home/uchenik$i/cat
-ln -s /bin/cd /home/uchenik$i/cd
-ln -s /bin/cd /home/uchenik$i/wc
-ln -s /bin/cd /home/uchenik$i/pwd
-ln -s /bin/cd /home/uchenik$i/cp
-ln -s /bin/cd /home/uchenik$i/nano
-echo 'PATH=$HOME/' >> /home/uchenik$i/.bashrc
-
+mkdir /home/filesystemTask/uchenik$i/hochu5
+python3 /root/makeManyFile.py $i /home/filesystemTask/uchenik$i/hochu5
+chmod -R a-w /home/filesystemTask/uchenik$i
+chown -R uchenik$i:uchenik$i /home/filesystemTask/uchenik$i
+ln -s /bin/ls /home/filesystemTask/uchenik$i/ls
+ln -s /bin/cat /home/filesystemTask/uchenik$i/cat
+ln -s /bin/wc /home/filesystemTask/uchenik$i/wc
+ln -s /bin/pwd /home/filesystemTask/uchenik$i/pwd
+ln -s /bin/cp /home/filesystemTask/uchenik$i/cp
+ln -s /bin/nano /home/filesystemTask/uchenik$i/nano
+echo 'PATH=$HOME/' >> /home/filesystemTask/uchenik$i/.bashrc
+echo 'export PATH' >> /home/filesystemTask/uchenik$i/.bashrc
 done
